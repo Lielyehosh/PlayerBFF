751e5f7bb30801f6702e9d0eee699cf642636a23
Liel <lielyehosh@gmail.com>
2021-09-11 13:11:32 +0300

    Initial commit with auth controller

---
+++ PlayerBFF/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PlayerBFF.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly Logger<AuthController> _logger;
+        // LOGIN
+        public AuthController(Logger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost,Route("login")]
+        public IActionResult LoginAsync([FromBody] LoginModel request, CancellationToken ct)
+        {
+            if (request == null)
+                return BadRequest("Invalid client request");
+
+            if (!string.IsNullOrEmpty(request.Username) && !string.IsNullOrEmpty(request.Password))
+            {
+                // TODO - validate password and user name in different scope
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+                var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "http://localhost:5000",
+                    audience: "http://localhost:5000",
+                    claims: new List<Claim>(),
+                    expires: DateTime.UtcNow.AddMinutes(5),
+                    signingCredentials: signingCredentials
+                );
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                return Ok(new {Token = tokenString});
+            }
+
+            return Unauthorized();
+        }
+    }
+}
---
+++ PlayerBFF/Controllers/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlayerBFF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<string> Get() => new string[] {"Sami", "Sami A"};
+    }
+}
