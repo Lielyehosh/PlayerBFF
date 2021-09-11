bfe9b135040fd2a910b6a7d30b809f12b2f157c2
Liel <lielyehosh@gmail.com>
2021-09-11 13:44:21 +0300

    Add ILogger

--- PlayerBFF/Controllers/AuthController.cs
+++ PlayerBFF/Controllers/AuthController.cs
@@ -14,9 +14,9 @@ namespace PlayerBFF.Controllers
     [ApiController]
     public class AuthController : Controller
     {
-        private readonly Logger<AuthController> _logger;
+        private readonly ILogger<AuthController> _logger;
         // LOGIN
-        public AuthController(Logger<AuthController> logger)
+        public AuthController(ILogger<AuthController> logger)
         {
             _logger = logger;
         }
@@ -29,13 +29,12 @@ namespace PlayerBFF.Controllers
 
             if (!string.IsNullOrEmpty(request.Username) && !string.IsNullOrEmpty(request.Password))
             {
-                // TODO - validate password and user name in different scope
                 var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
                 var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
                 var tokenOptions = new JwtSecurityToken(
-                    issuer: "http://localhost:5000",
-                    audience: "http://localhost:5000",
+                    issuer: "https://localhost:5001",
+                    audience: "https://localhost:5001",
                     claims: new List<Claim>(),
                     expires: DateTime.UtcNow.AddMinutes(5),
                     signingCredentials: signingCredentials
--- PlayerBFF/Startup.cs
+++ PlayerBFF/Startup.cs
@@ -41,8 +41,8 @@ namespace PlayerBFF
                         ValidateLifetime = true,
                         ValidateAudience = true,
                         ValidateIssuerSigningKey = true,
-                        ValidIssuer = "http://localhost:5000",
-                        ValidAudience = "http://localhost:5000",
+                        ValidIssuer = "https://localhost:5001",
+                        ValidAudience = "https://localhost:5001",
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"))
                     };
                 });
